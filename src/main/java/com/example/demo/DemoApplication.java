3ed7f27b127afbfa6765c899b74961588555c83e
wlgggy <jeehyo0202@naver.com>
2025-06-10 00:39:55 +0900

    ★[create] : spring boot project★

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner testDatabaseConnection(DataSource dataSource) {
+        return args -> {
+            try {
+                dataSource.getConnection().close();
+                logger.info("성공");
+            } catch (Exception e) {
+                logger.error("실패");
+            }
+        };
+    }
+}
